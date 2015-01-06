0b7e17ea48e43804297afeeb76fe19ddfce73a4a
Amra7 <amrapop@gmail.com>
2015-01-06 11:38:50 +0100

    Complete homework "Gui Binary" task

--- src/vjezbe/GuiBinary.java
+++ src/vjezbe/GuiBinary.java
@@ -4,49 +4,81 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class GuiBinary {
+public class GuiBinary implements ActionListener{
 	
-	static JTextField num;
-	static int numForGuess = (int)(1+Math.random() * 10000);
-	static JTextArea area;
+	static JTextField num= new JTextField(30);
+	static JButton b1= new JButton("Less");
+	static JButton b2= new JButton("Correct");
+	static JButton b3= new JButton("More");
+	
+	static int end = 10000;
+	static int start = 0;
+	static int mid = (start+end)/2;
+	static String str;
+	static int counter =0;
+	
 
 	public static void main(String[] args) {
 		
 		JFrame window = new JFrame("Pogodi broj");
-		window.setSize(500,500);
+		window.setSize(400,300);
 				
 		Draw panel = new Draw();
-		panel.setSize(500,500);
+		panel.setSize(400,300);
 		panel.setLayout(new FlowLayout());
 		
-		num = new JTextField(40);
-		num.setSize(100, 30);
-		num.getAccessibleContext();
-		panel.add(num);
-	
-		ButtonHandler listener = new ButtonHandler();
+		mid = (start+end)/2;
+		str= Integer.toString(mid);
+		num.setText(str);
+		panel.add(num);	
 		
-		JButton b1= new JButton("Manje");
 		panel.add(b1);
-		b1.addKeyListener(listener);
-    	JButton b2= new JButton("Tacno");
-    	b1.addKeyListener(listener);
+		b1.addActionListener(new ActionListener (){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				counter++;
+				end = mid -1;
+				mid = (start + end)/2;
+				str = Integer.toString(mid);
+				num.setText(str);
+				
+			}
+			
+		});
 		panel.add(b2);
-		JButton b3= new JButton("Vise");
-		b1.addKeyListener(listener);
+		b2.addActionListener(new ActionListener (){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				return;
+			}
+			
+		});
 		panel.add(b3);
-		
-		area = new JTextArea(5,30);
-		panel.add(area);
+		b3.addActionListener(new ActionListener (){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				counter++;
+				start = mid+1;
+				mid = (start + end)/2;
+				str = Integer.toString(mid);
+				num.setText(str);
+				System.out.println("Start: " + start);
+				System.out.println("End: " + end);
+				System.out.println("Mid: " + mid);
+				System.out.println("Str: " + str);
+			}
+			
+		});
 		
 		window.setContentPane(panel);
 		
@@ -57,65 +89,17 @@ public class GuiBinary {
 	public static class Draw extends JPanel {
 		public void paintComponent(Graphics g){
 			super.paintComponent(g);
-			g.drawString("Unesi jedan broj izmedju 1 i 10 000", 500, 100);
+			g.drawString("Imagine number between 1 and 10.000",90, 100);
 		}
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
-	public static class  ButtonHandler implements KeyListener, ActionListener {
 
-		 int numGuess;
-		
-		@Override
-		public void keyPressed(KeyEvent arg0) {
-			binarySearchRecursive(numForGuess, num, 0, 10000);
-			
-		}
-
-		@Override
-		public void keyReleased(KeyEvent arg0) {
-			binarySearchRecursive(numForGuess, num, 0, 10000);
-			
-		}
-
-		@Override
-		public void keyTyped(KeyEvent arg0) {
-			binarySearchRecursive(numForGuess, num, 0, 10000);
-			
-		}
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			numGuess = Integer.parseInt(num.getText());
-			
-		}
-		
-	}
-	
-	/**
-	 * Binary search with recursion.
-	 * @param nums - array of numbers.
-	 * @param num - number that we enter.
-	 * @param start - start of unsorted array.
-	 * @param end - end of unsorted array.
-	 * @return index of number that we are searching in array of nums.
-	 */
-	private static int binarySearchRecursive( int numForGuess, JTextField num, int start, int end){
-		int numGuess = Integer.parseInt(num.getText());
-		if( start >= end){
-			return -1;
-		}		
-		int mid = (start + end)/2;
-		
-		if (numGuess == numForGuess){
-			return mid;
-		} else if (numGuess < numForGuess) {
-			return binarySearchRecursive(numForGuess, num, start, mid);
-		} else if( numGuess > numForGuess){
-			return binarySearchRecursive(numForGuess, num, mid+1, end);
-		}
-		
-		return -1;
-	}
 
 }
